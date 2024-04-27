018e76043e428938c1b58a91a710026b6afc9ff4
ddaniellim <limdeux7@gmail.com>
2024-04-27 13:34:01 +0800

    first test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import "./App.css";
+import Test from "./test";
 
 function App() {
   return (
     <div className="App">
-      
+      <Test name="Bryan Garate" salary={100000} position="developer" company="orro"/>
+      <Test name="Deux Lim" salary={31000} position="developer" company="orro"/>
+      <Test name="Nicole Zuniega" salary={60000} position="developer" company="orro"/>
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,12 @@
+const Test = (props) => {
+    return(
+        <>
+            <h1>{props.name}</h1>
+            <h1>{props.salary}</h1>
+            <h1>{props.position}</h1>
+            <h1>{props.company}</h1>
+        </>
+    )
+}
+
+export default Test;
