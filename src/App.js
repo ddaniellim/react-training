927f0524afa58074869e0b028ac44d3aefc95fa5
ddaniellim <limdeux7@gmail.com>
2024-04-28 13:39:54 +0800

    added axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+import Axios from 'axios'
 //import {useState} from "react"; 
 
 //import Test from "./test";
-import { TodoList } from "./todo";
+//import { TodoList } from "./todo";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      
     </div>
   );
 }
