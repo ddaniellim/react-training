107d03b310d15ce3f482d72a9d31cee999f54ec0
ddaniellim <limdeux7@gmail.com>
2024-05-02 15:33:02 +0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import "./App.css";
-import Axios from 'axios'
-//import {useState} from "react"; 
-
-//import Test from "./test";
-//import { TodoList } from "./todo";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home'; // Update import statement
+import { Menu } from './pages/Menu'; // Update import statement
+import { Contact } from './pages/Contact'; // Update import statement
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+          <Link to="/react-training">Home</Link> <br/>
+          <Link to="/react-training/Menu">Menu</Link> <br/>
+          <Link to="/react-training/Contact">Contact</Link>
+          
+        <Routes>
+          <Route path="/react-training" element={<Home />} /> 
+          <Route path="/react-training/Menu" element={<Menu />} /> 
+          <Route path="/react-training/Contact" element={<Contact />} /> 
+          <Route path="*" element={<div> PAGE NOT FOUND</div>} /> 
+        </Routes>
+      </Router>
     </div>
   );
 }
